9f6009dd706003a874bb1f9effa0d80290783098
Shuyan Liu <shuyanliu@pc-5-68.customer.ask4.lan>
2021-09-14 15:42:42 +0100

    add name and AND title

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
-  const [roomName, setRoomName] = React.useState("");
+  const [roomName, setRoomName, userName, andTitle] = React.useState("");
 
   const handleRoomNameChange = (event) => {
     setRoomName(event.target.value);
   };
+
 
   return (
     <div className="home-container">
@@ -17,6 +18,18 @@ const Home = () => {
         placeholder="Room"
         value={roomName}
         onChange={handleRoomNameChange}
+        className="text-input-field"
+      />
+      <input
+        type="text"
+        placeholder="Name"
+        value={userName}
+        className="text-input-field"
+      />
+      <input
+        type="text"
+        placeholder="AND Title"
+        value={andTitle}
         className="text-input-field"
       />
       <Link to={`/${roomName}`} className="enter-room-button">
